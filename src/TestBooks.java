aefab212e5c851cf885238efeb48a7379e1258fd
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2023-10-09 19:03:02 -0400

    comaring books and getting the smallest number of pages in a Book

--- src/TestBooks.java
+++ src/TestBooks.java
@@ -5,7 +5,8 @@ public class TestBooks {
     public static void main(String[] args) {
 //        Scanner part for Input
         Scanner scanner = new Scanner(System.in);
-        Book booksForUser = new Book();
+
+        int NumPages = 0;
 
         List<Book> BooksReadSummer = new ArrayList<Book>();
 
@@ -14,17 +15,40 @@ public class TestBooks {
         int numberOfBooks = scanner.nextInt();
         System.out.println("Thank you, please write the information about the books. ");
         for(int i = 0;i < numberOfBooks; i ++){
-            System.out.println("Enter the title of the book");
+            System.out.println("Enter the title of the book Number: " +(i+1));
             String titleUser = scanner.next();
-            System.out.println("Enter the number of pages for your book");
+            System.out.println("Enter the number of pages for your book Number: " +(i+1));
             int pagesUser = scanner.nextInt();
+            Book booksForUser = new Book();
             booksForUser.initBook(pagesUser,titleUser);
             BooksReadSummer.add(booksForUser);
+            System.out.println("===============================================");
         }
 
+        System.out.println("Books read during the Summer");
         for (Book book : BooksReadSummer){
             System.out.println(book.toString());
         }
+//          Average number of pages
+        for(Book book: BooksReadSummer){
+            NumPages = NumPages+book.getPages();
+        }
+        int totalPages = NumPages/numberOfBooks;
+        System.out.println("Average number of pages per book read:");
+        System.out.println("===========================================");
+        System.out.println("Your average is: "+totalPages +" Pages");
+//        Get the Smallest number of pages in a Book
+        Book SmallPages = new Book();
+
+        int lastBook = BooksReadSummer.size()-1;
+
+        for(int i = 0; i<lastBook; i++){
+            int compare = SmallPages.CompareTo(BooksReadSummer.get(i),BooksReadSummer.get(i+1));
+            if (compare == -1){
+                BooksReadSummer.set(i+1,BooksReadSummer.get(i));
+            }
+        }
+        System.out.println("the Smallest number of pages in a Book is: " +BooksReadSummer.get(lastBook));
 
     }
 }
